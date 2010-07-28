15aff3fc8ddf7c45f306e111f3f9cf44ac3b6330
Christian Spanring <cspanring@gmail.com>
2010-07-28 14:33:27 -0400

    changed list view for user profiles

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 class UserProfileAdmin(admin.ModelAdmin):
     fields = ['user','town', 'position']
+    list_display = ('user', 'town', 'position')
 
 admin.site.register(UserProfile, UserProfileAdmin)
