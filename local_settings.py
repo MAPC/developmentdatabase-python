b371a1aab8793b71b8b848c9c382189f35ac46fb
Ben Holland <bholland@mapc.org>
2012-03-27 14:49:43 -0400

    Adding my own local settigns

---
+++ local_settings.py
@@ -0,0 +1,14 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'development',                      # Or path to database file if using sqlite3.
+        'USER': 'ben',                      # Not used with sqlite3.
+        'PASSWORD': 'password',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+GOOGLE_MAPS_KEY=''
+
+TEMPLATE_DIRS = ("/vagrant/git/developmentdatabase/templates")
