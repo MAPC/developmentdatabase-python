bf513b8afd858d219fedc3cecf8a0b171586fbad
Christian Spanring <cspanring@gmail.com>
2012-03-28 03:07:09 -0400

    adds filter page prototype

--- urls.py
+++ urls.py
@@ -1,6 +1,10 @@
 from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 
+from django.views.generic import TemplateView
+
+
+
 # from django.views.generic.simple import direct_to_template
 
 from registration.views import register
@@ -9,13 +13,26 @@ from registration.views import register
 from django.contrib import admin
 admin.autodiscover()
 
+# API
+from tastypie.api import Api
+from projects.api import ProjectResource
+
+v1_api = Api(api_name='v1')
+v1_api.register(ProjectResource())
+
+
+
 urlpatterns = patterns('',
     # Example:
-	url(r'^$', 'projects.views.index', name='home'),
+    url(r'^$', 'projects.views.index', name='home'),
+
+    
+    # ('^project/filter/$', TemplateView.as_view(template_name='projects/filter.html')),
+    ('^project/filter/$', 'projects.views.filter'),
     
     (r'^project/(?P<project_id>\d+)/$', 'projects.views.detail'),
     (r'^project/(?P<project_id>\d+)/edit/$', 'projects.views.edit'),
-	(r'^project/add/$', 'projects.views.add'),
+    (r'^project/add/$', 'projects.views.add'),
     # (r'^project/(?P<project_id>\d+)/save/$', 'communitycomments.projects.views.save'),
     (r'^project/(?P<project_id>\d+)/geojson/$', 'projects.views.project_geojson'),
     
@@ -26,6 +43,9 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
+
+    # API
+    (r'^api/', include(v1_api.urls)),
     
     
     (r'^accounts/', include('accounts.urls')),
@@ -34,8 +54,8 @@ urlpatterns = patterns('',
     (r'^(?P<town_name>\w+)/taz/geojson/$', 'projects.views.town_taz_geojson'),
     (r'^(?P<town_name>\w+)/$', 'projects.views.community'),
     # (r'^(?P<community_name>\w+)/$', 'communitycomments.projects.views.community')
-	# (r'^game', direct_to_template,
-    #        { 'template': 'game.html' }, 'game'	),
+    # (r'^game', direct_to_template,
+    #        { 'template': 'game.html' }, 'game'    ),
 )
 
 if settings.DEBUG:
