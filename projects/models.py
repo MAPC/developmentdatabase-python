c620242b2d1e63099439bd767815d7a90bbef9b5
Christian Spanring <cspanring@gmail.com>
2012-04-22 13:27:44 -0400

    updates project model class
    adds legacy fields, reorganizes lookup tables and choice fields, cleans
    custom save

--- projects/models.py
+++ projects/models.py
@@ -56,117 +56,130 @@ class Taz(models.Model):
     def __unicode__(self):
         return self.taz_id
 
-class StatusChoice(models.Model):
-    status = models.CharField(max_length=50, blank=False, null=False)
-    
-class ZoningChoice(models.Model):
-    type = models.CharField(max_length=20, blank=False, null=False)
-    description = models.CharField(max_length=255, blank=False, null=False)
 
-class TypeChoice(models.Model):
-    type = models.CharField(max_length=50, blank=False, null=False)
+class ProjectStatus(models.Model):
+    status = models.CharField(max_length=50)
+
+    
+class ZoningTool(models.Model):
+    z_type = models.CharField('Type', max_length=5)
+    description = models.TextField(blank=True, null=True)
+
+
+class ProjectType(models.Model):
+    p_type = models.CharField('Type', max_length=50)
+
     
 class Project(models.Model):
-    # taz = models.IntegerField('TAZ')
+    """
+    An economic or housing development project.
+    """
+
+    YESNO_CHOICES = (
+        ('u', 'Unknown'), 
+        ('y', 'Yes'), 
+        ('p', 'Partial'),
+        ('n', 'No'),
+    )
+
+    taz = models.ForeignKey(Taz, blank=True, null=True)
     dd_id = models.IntegerField(primary_key=True)
-    name = models.CharField("Project Name (DDname)", max_length=1000)
-    description = models.TextField("Project Description ", blank=True, null=True)
-    type_id = models.ForeignKey("Project Type (DvtType)", TypeChoice, blank=True, null=True)
-    type_detail = models.TextField("Project Type Detail", blank=True, null=True)
-    status_id = models.ForeignKey("Project Status (Status)", StatusChoice, blank=True, null=True)
-    stalled = models.TextField("Project Stalled", blank=True, null=True)
-    phase = models.TextField("Project Phase", blank=True, null=True)
-    completion = models.IntegerField("Estimated or Actual Year of Completion (ComplYr)", blank=True, null=True)
-    area = models.FloatField("Project Area in Acres (PrjAcrs)", blank=True, null=True)
-    dev_name = models.TextField("Developer Name", blank=True, null=True)
-    website = models.URLField("Project Website (DD_URL)", max_length=1000, blank=True, null=True)
-    website_add = models.URLField("Additional Website", max_length=1000, blank=True, null=True)
-    created_by = models.ForeignKey("Added By", User, editable=False, related_name="project_created_by", blank=True, null=True)
-    create_date = models.DateTimeField("Create Date", editable=False, auto_now=True)
-    last_updated_by = models.ForeignKey("Last updated by", User, related_name="project_last_updated_by", blank=True, null=True)
-    last_modified = models.DateTimeField("Last updated date", editable=False, auto_now=True)
+    name = models.CharField('Project Name (DDname)', max_length=255)
+    description = models.TextField('Project Description ', blank=True, null=True)
+    p_type = models.ForeignKey(ProjectType, blank=True, null=True)
+    p_type_detail = models.TextField('Project Type Detail', blank=True, null=True)
+    status = models.ForeignKey(ProjectStatus, blank=True, null=True)
+    stalled = models.TextField('Project Stalled', blank=True, null=True)
+    phase = models.TextField('Project Phase', blank=True, null=True)
+    completion = models.IntegerField('Estimated or Actual Year of Completion (ComplYr)', blank=True, null=True)
+    area = models.FloatField('Project Area in Acres (PrjAcrs)', blank=True, null=True)
+    dev_name = models.TextField('Developer Name', blank=True, null=True)
+    website = models.URLField('Project Website (DD_URL)', max_length=1000, blank=True, null=True)
+    website_add = models.URLField('Additional Website', max_length=1000, blank=True, null=True)
+    created_by = models.ForeignKey(User, editable=False, related_name='project_created_by', blank=True, null=True)
+    create_date = models.DateTimeField('Create Date', editable=False, auto_now=True)
+    last_updated_by = models.ForeignKey(User, related_name='project_last_updated_by', blank=True, null=True)
+    last_modified = models.DateTimeField('Last updated date', editable=False, auto_now=True)
     
     #housing
-    total_housing_units = models.IntegerField("Total Housing Units (TotHU)", blank=True, null=True)
-    detached_single_fam= models.IntegerField("Detached Single Family (SingFamHU)", blank=True, null=True)
-    townhouse_small_multi_fam = models.IntegerField("Townhouse and Small Multifamily (TwnhSmMult)", blank=True, null=True)
-    med_large_multi_fam = models.IntegerField("Medium and Large Multifamily (LgMultiFam)", blank=True, null=True)
-    age_restricted_pct = models.CharField("Percent Age Restricted (Ovr55)", max_length=20, blank=True, null=True, choices=(("0", "Unknown"), ("YES", "Yes"), ("NO", "No")))
-    affordable_pct = models.FloatField("Percent Affordable (PctAffAll)", blank=True, null=True)
-    affordable_comment = models.TextField("Affordability Comments", blank=True, null=True)
-    group_quarters = models.IntegerField("Group Quarters (GQPOP)", blank=True, null=True)
+    total_housing_units = models.IntegerField('Total Housing Units (TotHU)', blank=True, null=True)
+    detached_single_fam= models.IntegerField('Detached Single Family (SingFamHU)', blank=True, null=True)
+    townhouse_small_multi_fam = models.IntegerField('Townhouse and Small Multifamily (TwnhSmMult)', blank=True, null=True)
+    med_large_multi_fam = models.IntegerField('Medium and Large Multifamily (LgMultiFam)', blank=True, null=True)
+    age_restricted_pct = models.CharField('Percent Age Restricted (Ovr55)', max_length=1, blank=True, null=True, choices=YESNO_CHOICES)
+    affordable_pct = models.FloatField('Percent Affordable (PctAffAll)', blank=True, null=True)
+    affordable_comment = models.TextField('Affordability Comments', blank=True, null=True)
+    group_quarters = models.IntegerField('Group Quarters (GQPOP)', blank=True, null=True)
     
     #total non-res
-    nonres_dev = models.IntegerField("Total Nonresidential Development in Square Feet (CommSF)", blank=True, null=True)
-    hotel_rooms = models.IntegerField("Hotel rooms (HotelRms)", blank=True, null=True)
-    retail_restaurant_pct = models.FloatField("Retail / Restaurant Percent (RetPct)", blank=True, null=True)
-    office_medical_pct = models.FloatField("Office / Medical Percent (OfcMdPct)", blank=True, null=True)
-    manufacturing_industrial_pct = models.FloatField("Manufacturing / Industrial Percent (IndMfPct)", blank=True, null=True)
-    warehouse_trucking_pct = models.FloatField("Warehouse / Trucking Percent (WhsPct)", blank=True, null=True)
-    lab_RandD_pct = models.FloatField("Lab / R & D  Percent (RnDPct)", blank=True, null=True)
-    edu_institution_pct = models.FloatField("Edu/Institution Percent (EdInstPct)", blank=True, null=True)
-    other_pct = models.FloatField("Other (OthPct)", blank=True, null=True)
+    nonres_dev = models.IntegerField('Total Nonresidential Development in Square Feet (CommSF)', blank=True, null=True)
+    hotel_rooms = models.IntegerField('Hotel rooms (HotelRms)', blank=True, null=True)
+    retail_restaurant_pct = models.FloatField('Retail / Restaurant Percent (RetPct)', blank=True, null=True)
+    office_medical_pct = models.FloatField('Office / Medical Percent (OfcMdPct)', blank=True, null=True)
+    manufacturing_industrial_pct = models.FloatField('Manufacturing / Industrial Percent (IndMfPct)', blank=True, null=True)
+    warehouse_trucking_pct = models.FloatField('Warehouse / Trucking Percent (WhsPct)', blank=True, null=True)
+    lab_RandD_pct = models.FloatField('Lab / R & D  Percent (RnDPct)', blank=True, null=True)
+    edu_institution_pct = models.FloatField('Edu/Institution Percent (EdInstPct)', blank=True, null=True)
+    other_pct = models.FloatField('Other (OthPct)', blank=True, null=True)
 
     #jobs
-    jobs = models.IntegerField("Estimated Employment (RptdEmp / EmpLoss)", blank=True, null=True)
-    est_emp = models.FloatField("Other jobs per 1000 sqft", blank=True, null=True)
+    jobs = models.IntegerField('Estimated Employment (RptdEmp / EmpLoss)', blank=True, null=True)
+    est_emp = models.FloatField('Other jobs per 1000 sqft', blank=True, null=True)
     est_emp_loss = models.IntegerField(blank=True, null=True)
     jobs_per_1000 = models.IntegerField(blank=True, null=True) # jobs per 1000 sq. ft.
-    metro_future_discount_pct = models.FloatField("Metro Future Discount (MFDisc)", blank=True, null=True)
-    current_trends_discount_pct = models.FloatField("Current trends discount", blank=True, null=True)
+    metro_future_discount_pct = models.FloatField('Metro Future Discount (MFDisc)', blank=True, null=True)
+    current_trends_discount_pct = models.FloatField('Current trends discount', blank=True, null=True)
     
     #project specific
-    parking_spaces = models.IntegerField("Parking Spaces", blank=True, null=True)
-    redevelopment = models.CharField("Redevelopment (Rdv)", max_length=20, blank=True, null=True, choices=(("0", "Unknown"), ("YES", "Yes"), ("NO", "No")))
-    cluster_subdivision = models.CharField("Cluster Subdivision (ClustOSRD)", max_length=20, blank=True, null=True, choices=(("0", "Unknown"), ("YES", "Yes"), ("NO", "No")))
-    zoning_tool_id = models.ForeignKey("Zoning Tool (Ch40)", ZoningChoice, blank=True, null=True)
-    as_of_right = models.TextField("As-of-Right", blank=True, null=True)
-    mixed_use = models.CharField("Mixed use project (MxdUse)", max_length=20, blank=True, null=True, choices=(("0", "Unknown"), ("YES", "Yes"), ("NO", "No")))
-    total_cost = models.IntegerField("Total project cost", blank=True, null=True)
-    total_cost_allocated_pct = models.FloatField("Funding allocated Percent", blank=True, null=True)
-    comment = models.TextField("Comments DD_info", blank=True, null=True)
-    mapc_comment = models.TextField("MAPC comments", blank=True, null=True)
-    
-    # GeoDjango-specific: a geometry field and overriding 
-    # the default manager with a GeoManager instance.
+    parking_spaces = models.IntegerField('Parking Spaces', blank=True, null=True)
+    redevelopment = models.CharField('Redevelopment (Rdv)', max_length=1, blank=True, null=True, choices=YESNO_CHOICES)
+    cluster_subdivision = models.CharField('Cluster Subdivision (ClustOSRD)', max_length=1, blank=True, null=True, choices=YESNO_CHOICES)
+    zoning_tool = models.ForeignKey(ZoningTool, blank=True, null=True)
+    as_of_right = models.TextField('As-of-Right', blank=True, null=True)
+    mixed_use = models.CharField('Mixed use project (MxdUse)', max_length=1, blank=True, null=True, choices=YESNO_CHOICES)
+    total_cost = models.IntegerField('Total project cost', blank=True, null=True)
+    total_cost_allocated_pct = models.FloatField('Funding allocated Percent', blank=True, null=True)
+    comment = models.TextField('Comments DD_info', blank=True, null=True)
+    mapc_comment = models.TextField('MAPC comments', blank=True, null=True)
+    draft = models.BooleanField(help_text='Required project information is incomplete.')
+    removed = models.BooleanField(help_text='Deleted project, will not be shown on public page')
+
+    # legacy fields, to be evaluated and then removed or merged into new fields
+    legacy_status = models.CharField('Legacy: Status', max_length=255, blank=True, null=True, help_text='Completed, Under Construction, Planning, or Projected')
+    legacy_mf_hu = models.FloatField('Legacy: Housing units after discounting', blank=True, null=True)
+    legacy_ch40 = models.CharField('Legacy: 40B/40R/43D', max_length=255, blank=True, null=True, help_text='Indicates use of Chapter 40B (Comprehensive Permit law), 40R (Smart Growth Zoning), or 43D (Expedited Permitting)')
+    legacy_totempl = models.FloatField('Legacy: Total Employment Potential', blank=True, null=True, help_text='MAPC estimates of total potential employment based on reported employment, square footage, and standard employment density factors')
+    legacy_mf_empl = models.FloatField('Legacy: Discounted Employment', blank=True, null=True, help_text='Total employment after applying discount factor to projects not yet completed, MetroFuture Scenario')
+    legacy_dvttype = models.CharField('Legacy: Type of Project', max_length=255, blank=True, null=True)
+    legacy_mftot00_10 = models.FloatField('Legacy: Employment 2000 - 2010', blank=True, null=True)
+    legacy_mftot10_20 = models.FloatField('Legacy: Employment 2010 - 2020', blank=True, null=True)
+    legacy_mftot20_30 = models.FloatField('Legacy: Employment 2020 - 2030', blank=True, null=True)
+    legacy_mftot30_35 = models.FloatField('Legacy: Employment 2030 - 2035', blank=True, null=True)
+    legacy_mfhu00_10 = models.FloatField('Legacy: Housing Units 2000 - 2010', blank=True, null=True)
+    legacy_mfhu10_20 = models.FloatField('Legacy: Housing Units 2010 - 2020', blank=True, null=True)
+    legacy_mfhu20_30 = models.FloatField('Legacy: Housing Units 2020 - 2030', blank=True, null=True)
+    legacy_mfhu30_35 = models.FloatField('Legacy: Housing Units 2030 - 2035', blank=True, null=True)
+    legacy_mfgq00_10 = models.FloatField('Legacy: Group Quarters 2000 - 2010', blank=True, null=True)
+    legacy_mfgq10_20 = models.FloatField('Legacy: Group Quarters 2010 - 2020', blank=True, null=True)
+    legacy_mfgq20_30 = models.FloatField('Legacy: Group Quarters 2020 - 2030', blank=True, null=True)
+    legacy_mfgq30_35 = models.FloatField('Legacy: Group Quarters 2030 - 2035', blank=True, null=True)
+
+    # geometry
     location = models.PointField(srid=26986) # SRS mass state plane
     objects = models.GeoManager()
-
-    taz_id = models.ForeignKey(Taz, to_field='taz_id', editable=True, blank=True, null=True)
     
-    # find taz for project
+    # custom save
     def save(self, *args, **kwargs):
-#        # try find taz, otherwise don't validate form! ...probably move to form_save
-        # l = 'POINT (243617.8433000145596452 901646.1310999535489827)'
-#        t = Taz.objects.get(geometry__contains=l)
-#        self.taz = t
-         # dummy until we figure another solution out
-        # if not self.id:
         try:
+            # find and cache TAZ for project location
             self.taz = Taz.objects.get(geometry__contains=self.location)
         except:
-            self.taz = None
-            # geometry__contains=self.location
-        #    self.last_modified = datetime.date.today()
-        # self.comments = self.location
-        
-        self.last_modified = datetime.datetime.today()
-        # return super(Entry, self).save()
-        
-        
+            self.taz = None        
         super(Project, self).save(*args, **kwargs)
-        
-# topology rule
-#    def clean(self): 
-#        tv = Taz.objects.filter(geometry__contains=self.location)
-#        if len(tv) <= 1:
-#            raise ValidationError(len('Location is not within a TAZ. Please re-locate project.'))
-           
-    # So the model is pluralized correctly in the admin.
+                   
     class Meta:
-        verbose_name_plural = "Projects"
+        verbose_name_plural = 'Projects'
 
-    # Returns the string representation of the model.
     def __unicode__(self):
         return self.name
     
@@ -208,14 +221,14 @@ class Project(models.Model):
         return fields
 
 
-class ProjectForm(ModelForm):
-    class Meta:
-        model = Project
-        # fields = ['name', 'status', 'comments', 'location']
-        exclude = ['taz', 'removed']
-        widgets = {
-           'comments': Textarea(attrs={'cols': 80, 'rows': 20}),
-           'location': HiddenInput(),
-           'confirmed_by': HiddenInput(), 
-           'located_by': HiddenInput(),
-        }
+# class ProjectForm(ModelForm):
+#     class Meta:
+#         model = Project
+#         # fields = ['name', 'status', 'comments', 'location']
+#         exclude = ['taz', 'removed']
+#         widgets = {
+#            'comments': Textarea(attrs={'cols': 80, 'rows': 20}),
+#            'location': HiddenInput(),
+#            'confirmed_by': HiddenInput(), 
+#            'located_by': HiddenInput(),
+#         }
