c5ccf8678c1b8a61e688e18cb84c00fc51c95dce
Matt Cloyd <cloydster@gmail.com>
2014-01-25 13:30:24 -0500

    Change index page to include Project count

--- developmentdatabase/context_processors.py
+++ developmentdatabase/context_processors.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from development.models import Project
 
 def template_settings(request):
     """ Global values to pass to templates """
@@ -7,5 +8,6 @@ def template_settings(request):
         WS_MORE_INFO_ICON = settings.WS_MORE_INFO_ICON,
         WS_MORE_INFO_LINK = settings.WS_MORE_INFO_LINK,
         WS_LOGO_URL = settings.WS_LOGO_URL,
+        project_count = Project.objects.count(),
     ) 
         
